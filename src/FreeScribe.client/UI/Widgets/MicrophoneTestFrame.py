0942708aae8047c530f9c2b76451954602773b15
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2025-03-13 13:34:36 -0400

    filter to show microphones only

--- src/FreeScribe.client/UI/Widgets/MicrophoneTestFrame.py
+++ src/FreeScribe.client/UI/Widgets/MicrophoneTestFrame.py
@@ -67,10 +67,9 @@ class MicrophoneTestFrame:
         device_count = self.p.get_device_count()
         for i in range(device_count):
             device_info = self.p.get_device_info_by_index(i)
-            if device_info['maxInputChannels'] > 0:
+            if device_info['maxInputChannels'] > 0 and device_info['hostApi'] == 0:
                 device_name = device_info['name']
-                excluded_names = ["Virtual", "Output", "Wave Out", "What U Hear", "Aux", "Port"]
-                if not any(excluded_name.lower() in device_name.lower() for excluded_name in excluded_names) and device_name not in [name for _, name in self.mic_list]:
+                if device_name not in [name for _, name in self.mic_list]:
                     self.mic_list.append((i, device_name))
                     self.mic_mapping[device_name] = i
         # Load the selected microphone from settings if available
