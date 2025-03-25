6a3f4a32b8f8e0129ebdfebc0dc1d76ee2ae9cbd
Alex Simko <alexsimko4@gmail.com>
2025-03-25 12:43:56 -0400

    fix(loaded-file): preloaded file to correct format before passing it to whisper.
    loaded data to np.ndarray format before passing it to whisper. to prevent ffmpeg errors.

--- src/FreeScribe.client/client.py
+++ src/FreeScribe.client/client.py
@@ -937,10 +937,16 @@ def send_audio_to_server():
         # Display a message indicating that audio to text processing is in progress
         user_input.scrolled_text.insert(tk.END, "Audio to Text Processing...Please Wait")
         try:
-            # Determine the file to send for transcription
-            file_to_send = uploaded_file_path or get_resource_path('recording.wav')
-            delete_file = False if uploaded_file_path else True
-            uploaded_file_path = None
+            if utils.system.is_windows():
+                # Determine the file to send for transcription
+                file_to_send = uploaded_file_path or get_resource_path('recording.wav')
+                delete_file = False if uploaded_file_path else True
+                uploaded_file_path = None
+            else:
+                # Determine the file to send for transcription
+                file_to_send = uploaded_file_path
+                delete_file = False
+                uploaded_file_path = None
 
             # load stt model for transcription
             if not is_whisper_valid() and app_settings.is_low_mem_mode():
@@ -1512,12 +1518,18 @@ def generate_note_thread(text: str):
 
     root.after(500, lambda: check_thread_status(thread, loading_window))
 
-
+import librosa
 def upload_file():
     global uploaded_file_path
     file_path = filedialog.askopenfilename(filetypes=(("Audio files", "*.wav *.mp3 *.m4a"),))
     if file_path:
         uploaded_file_path = file_path
+        
+        # convert to np,ndarray
+        y, sr = librosa.load(uploaded_file_path, sr=RATE, mono=True)
+
+        uploaded_file_path = y
+
         threaded_send_audio_to_server()  # Add this line to process the file immediately
     start_flashing()
 
