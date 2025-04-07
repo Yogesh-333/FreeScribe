b6193b6bf3629337ad54aae1623f70faf7536770
Naitik Patel <n41tik@users.noreply.github.com>
2025-04-07 13:22:00 -0400

    adding comments

--- src/FreeScribe.client/utils/whisper/WhisperModel.py
+++ src/FreeScribe.client/utils/whisper/WhisperModel.py
@@ -253,6 +253,7 @@ def _faster_whisper_transcribe_macos(audio, app_settings):
     # Remove silent chunks
     cleaned_audio = _remove_silent_chunks(audio)
 
+    # passing arguments to translate
     generate_kwargs = {}
     if app_settings.editable_settings[SettingsKeys.USE_TRANSLATE_TASK.value]:
         generate_kwargs['task'] = 'translate'
