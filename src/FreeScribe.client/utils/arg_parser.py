f656e218f8cf6b8a37a48683e1696c7c2a015216
Alex Simko <alexsimko4@gmail.com>
2025-04-11 12:51:54 -0400

    feat(arg-parser): added a arg parser and a --file-debug argument to launch with file loger

--- src/FreeScribe.client/utils/arg_parser.py
+++ src/FreeScribe.client/utils/arg_parser.py
@@ -1,9 +1,10 @@
 import argparse
-from utils.log_config import logger
+import utils.log_config
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file-debug", help="Enable the file logger")
+    parser.add_argument("--file-debug", help="Enable the file logger", action="store_true")
     args = parser.parse_args()
 
-    logger.info(args)
+    if args.file_debug:
+        utils.log_config.add_file_handler(utils.log_config.logger, utils.log_config.formatter)
