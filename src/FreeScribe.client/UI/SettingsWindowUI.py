a2acc7b820f85028caa629010d666e0adb57154c
garciaerin <garciaerinjade@gmail.com>
2025-05-09 12:58:31 -0400

    fix: moved the whisper_language_codes to a constants module
    For better organization, a new file is created called "whisper_languages.py" under the folder called "constants" under FreeScribe.client.

--- src/FreeScribe.client/UI/SettingsWindowUI.py
+++ src/FreeScribe.client/UI/SettingsWindowUI.py
@@ -33,19 +33,8 @@ from UI.SettingsConstant import SettingsKeys, Architectures, FeatureToggle
 from UI.Widgets.PopupBox import PopupBox
 import utils.log_config
 from utils.log_config import logger
+from constants.whisper_languages import WHISPER_LANGUAGE_CODES
 
-WHISPER_LANGUAGE_CODES = [
-    "None (Auto Detect)",
-    "en", "zh", "de", "es", "ru", "ko", "fr", "ja", "pt", "tr", "pl", "ca", "nl", "ar",
-    "sv", "it", "id", "hi", "fi", "vi", "he", "uk", "el", "ms", "cs", "ro", "da", "hu",
-    "ta", "no", "th", "ur", "hr", "bg", "lt", "la", "mi", "ml", "cy", "sk", "te", "fa",
-    "lv", "bn", "sr", "az", "sl", "kn", "et", "mk", "br", "eu", "is", "hy", "ne", "mn",
-    "bs", "kk", "sq", "sw", "gl", "mr", "pa", "si", "km", "sn", "yo", "so", "af", "oc",
-    "ka", "be", "tg", "sd", "gu", "am", "yi", "lo", "uz", "fo", "ht", "ps", "tk", "nn",
-    "mt", "sa", "lb", "my", "bo", "tl", "mg", "as", "tt", "haw", "ln", "ha", "ba", "jw",
-    "su", "yue", "nan", "ceb", "hmn", "xh", "zu", "ny", "st", "gd", "sm", "chr", "co",
-    "haw", "ku", "lb", "fy", "gn", "ig", "jv", "kg", "rw", "sg", "tn", "to", "tw", "wo"
-]
 
 logger = logging.getLogger(__name__)
 
