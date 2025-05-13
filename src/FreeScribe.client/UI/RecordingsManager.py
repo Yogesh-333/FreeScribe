16edf84144907cc07e133b7be4769c0b3d8f2523
Alex Simko <alexsimko4@gmail.com>
2025-05-13 12:54:18 -0400

    feat(recordings-window): Add a no recordings  item in the list when none are present

--- src/FreeScribe.client/UI/RecordingsManager.py
+++ src/FreeScribe.client/UI/RecordingsManager.py
@@ -57,9 +57,18 @@ class RecordingsManager:
         # Populate recordings list
         if os.path.exists(get_resource_path("recordings")):
             recordings_dir = get_resource_path("recordings")
+            files_found = False
             for f in sorted(os.listdir(recordings_dir)):
                 if f.endswith('.AE2'):
                     self.recordings_list.insert('end', f)
+                    files_found = True
+            
+            if not files_found:
+                self.recordings_list.insert('end', "No recordings found")
+                self.recordings_list.itemconfig(0, {'fg': 'gray'})
+        else:
+            self.recordings_list.insert('end', "No recordings found")
+            self.recordings_list.itemconfig(0, {'fg': 'gray'})
 
         # Playback controls
         controls_frame = Frame(self.popup)
